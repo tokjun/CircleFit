9c351bbeb4c109162f4e37089390f3ae8df69187
Junichi Tokuda <tokuda@bwh.harvard.edu>
2015-04-02 10:38:14 -0400

    BUG: Fix bugs. Now the algorithm is working.

--- CircleFit.cxx
+++ CircleFit.cxx
@@ -29,7 +29,7 @@ typedef itk::Vector< double, 3 > VectorType;
 typedef itk::Statistics::ListSample< VectorType > PointListType;
 
 
-void CalcIntersectionOfPerpendicularBisectors2D(VectorType& p1, VectorType& p2, VectorType& p3, VectorType& intersec);
+int CalcIntersectionOfPerpendicularBisectors2D(VectorType& p1, VectorType& p2, VectorType& p3, VectorType& intersec, double radius);
   
 // Load points from the csv file. If successful, return > 0
 int LoadPoints(const char* filename, PointListType* points)
@@ -94,7 +94,7 @@ int main( int argc, char * argv [] )
   if( argc < 2 )
     {
     std::cerr << "Usage: " << std::endl;
-    std::cerr << argv[0] << " pointData " << std::endl;
+    std::cerr << argv[0] << " pointData [radius]" << std::endl;
     return EXIT_FAILURE;
     }
   const unsigned int Dimension = 2;
@@ -104,6 +104,11 @@ int main( int argc, char * argv [] )
   PointListType::Pointer points;
   points = PointListType::New();
 
+  double radius = -1.0;
+  if (argc > 2)
+    {
+    radius = atof(argv[2]);
+    }
 
   //----------------------------------------
   // Load the points
@@ -135,8 +140,8 @@ int main( int argc, char * argv [] )
                                          eigenValues, eigenMatrix );    
 
   // Print out the result of PCA
-  std::cerr << eigenValues << std::endl;
-  std::cerr << eigenMatrix << std::endl;
+  std::cout << eigenValues << std::endl;
+  std::cout << eigenMatrix << std::endl;
 
 
   //----------------------------------------
@@ -145,16 +150,16 @@ int main( int argc, char * argv [] )
   // the normal vector of the fitted plane.
 
   VectorType principalVector = eigenMatrix[0];
-  std::cerr << principalVector << std::endl;
+  std::cout << principalVector << std::endl;
 
   //----------------------------------------
   // Calculate the average position of the all points.
   // This is used as the origin of the new coordinate system on the
   // fitted plane.
 
-  CovarianceAlgorithmType::MeasurementVectorType meanVector;
-  meanVector = covarianceAlgorithm->GetMean();
-  std::cout << "Sample mean = " << meanVector << std::endl ; 
+  CovarianceAlgorithmType::MeasurementVectorType meanPoint;
+  meanPoint = covarianceAlgorithm->GetMean();
+  std::cout << "Sample mean = " << meanPoint << std::endl ; 
   
   //----------------------------------------
   // Project all the points to the fitted plane.
@@ -163,30 +168,35 @@ int main( int argc, char * argv [] )
   typedef PointListType::Iterator IteratorType;
   IteratorType iter = points->Begin();
 
+  VectorType nx =  eigenMatrix[2];
+  VectorType ny =  eigenMatrix[1];
+  VectorType nz =  eigenMatrix[0];
+  
   while (iter != points->End())
     {
     VectorType p1;
     VectorType p2;
-    p1 = iter.GetMeasurementVector() - meanVector;
-    p2[0] = p1*eigenMatrix[2];
-    p2[1] = p1*eigenMatrix[1];
-    //p2[2] = p1*eigenMatrix[0];
+    p1 = iter.GetMeasurementVector() - meanPoint;
+    p2[0] = p1*nx;
+    p2[1] = p1*ny;
+    //p2[2] = p1*nz;
     p2[2] = 0.0;
     transPoints->PushBack(p2);
-    //std::cout << "P2=("  << p2[0] << ", " << p2[1] << ", " << p2[2] << "); " << std::endl;
     ++ iter;
     }
 
-  // Pick up three points from the list and calculate the intersection of
-  // the perpendicular bisectors of the two chords connecting the three points.
+  // Pick up every combination of three points from the list and calculate
+  // the intersection of the perpendicular bisectors of the two chords connecting
+  // the three points.
 
-  // Pick up 3 points from the point set
+  VectorType meanIntersect;
+  meanIntersect[0] = 0.0;
+  meanIntersect[1] = 0.0;
+  meanIntersect[2] = 0.0;
+
   int nPoints = 0;
-  VectorType center;
-  center[0] = 0.0;
-  center[1] = 0.0;
-  center[2] = 0.0;
-  
+  int nPointsUsed = 0;
+
   for (IteratorType iter1 = transPoints->Begin(); iter1 != transPoints->End(); ++ iter1)
     {
     IteratorType iter2 = iter1;
@@ -199,20 +209,35 @@ int main( int argc, char * argv [] )
         VectorType p2 = iter2.GetMeasurementVector();
         VectorType p3 = iter3.GetMeasurementVector();
         VectorType c;
-        CalcIntersectionOfPerpendicularBisectors2D(p1, p2, p3, c);
-        center = center + c;
+
+        nPoints ++;
+        if (CalcIntersectionOfPerpendicularBisectors2D(p1, p2, p3, c, radius) > 0)
+          {
+          meanIntersect = meanIntersect + c;
+          nPointsUsed ++;
+          }
         }
       }
     }
-  center = center / nPoints;
-  std::cout << "Center=("  << center[0] << ", " << center[1] << ", " << center[2] << "); " << std::endl;
+
+  meanIntersect = meanIntersect / nPointsUsed;
+
+  // Transform the center point to the original coordinate system
+  VectorType center = meanIntersect[0] * nx + meanIntersect[1] * ny + meanIntersect[2] * nz + meanPoint;
+
+  std::cout << "Number of points: " << nPoints << std::endl;
+  std::cout << "Number of points used: " << nPointsUsed << std::endl;
+  std::cout << "Center = " << center << std::endl;
 
   return EXIT_SUCCESS;
 }
 
 
-void CalcIntersectionOfPerpendicularBisectors2D(VectorType& p1, VectorType& p2, VectorType& p3, VectorType& intersec)
+// Return 0, if the data set may not give a good estimate.
+int CalcIntersectionOfPerpendicularBisectors2D(VectorType& p1, VectorType& p2, VectorType& p3, VectorType& intersect, double radius)
 {
+
+  const double posErr = 5.0;
 
   // Compute the bisecting points between p1 and p2 (m1), and p2 and p3 (m2)
   VectorType m1 = (p1+p2)/2.0;
@@ -221,6 +246,12 @@ void CalcIntersectionOfPerpendicularBisectors2D(VectorType& p1, VectorType& p2, 
   // Compute the normal vectors along the perpendicular bisectors
   VectorType v12 = (p2-p1);
   VectorType v23 = (p3-p2);
+
+  if (v12.GetNorm() < posErr || v23.GetNorm() < posErr)
+    {
+    return 0;
+    }
+
   v12.Normalize();
   v23.Normalize();
 
@@ -233,7 +264,7 @@ void CalcIntersectionOfPerpendicularBisectors2D(VectorType& p1, VectorType& p2, 
   n2[0] = -v23[1];
   n2[1] = v23[0];
   n2[2] = v23[2];
-
+  
   // Compute the projection of m2 onto the perpendicular bisector of p1p2
   VectorType h = m1 + ((m2-m1)*n1)*n1;
 
@@ -244,12 +275,29 @@ void CalcIntersectionOfPerpendicularBisectors2D(VectorType& p1, VectorType& p2, 
   //
   // where 'a' is a scalar value. Projection of 'c' on the m2h is 'h'
   //
-  //    a * <n2> * (<h> - <m2>)/(|<h>-<m2>|) = |<h> - <m2>|
+  //    a * <n2> * (<h> - <m2>)/(|<h> - <m2>|) = |<h> - <m2>|
   //    a = |<h> - <m2>|^2 / {<n2> * (<h> - <m2>)}
   //
 
-  VectorType::RealValueType a = (h - m2)*(h - m2) / (n2 * (h - m2));
+  VectorType m2h = h-m2;
+  VectorType::RealValueType a = m2h.GetSquaredNorm() / (n2 * m2h);
   
-  intersec = m2 + a * n2;
-  
+  intersect = m2 + a * n2;
+
+  // Validate by radius, if radius is greater than 0
+  if (radius > 0)
+    {
+    VectorType d1 = p1-intersect;
+    VectorType d2 = p2-intersect;
+    VectorType d3 = p3-intersect;
+    
+    if (fabs(d1.GetNorm()-radius) > posErr ||
+        fabs(d2.GetNorm()-radius) > posErr ||
+        fabs(d3.GetNorm()-radius) > posErr)
+      {
+      return 0;
+      }
+    }
+
+  return 1;
 }
